46ed14c26c093fcd8477afdfb41b785c6ecd7637
Umer_pogrammer <78730094+umerprogramm@users.noreply.github.com>
2023-03-09 17:04:09 +0500

    Updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
-import { Provider } from 'react-redux'
 import '../styles/globals.css'
-import { Store } from '../store/Store'
 
 export default function App({ Component, pageProps }) {
   return( 
   <>
-  <Provider store={Store}>
   <Component {...pageProps} />
-  </Provider>
   </>
   )
 }
